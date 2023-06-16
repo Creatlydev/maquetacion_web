6a51d5b9066bc8606e8db3e72089c9130e401d7f
Creatlydev <creatlydev@gmail.com>
2023-06-16 08:59:30 -0500

    Update navbar 01

--- ui_components_design/navbar_01/js/container-resize.js
+++ ui_components_design/navbar_01/js/container-resize.js
@@ -2,11 +2,11 @@ export const observer = (element) => {
   function handleResize() {
     const currentWidth = element.offsetWidth;
     if (currentWidth < 960) {
-      document.getElementById('nav').style.display = 'none';
-      document.querySelector('.icon').style.display = 'block';
+      document.getElementById("nav").style.display = "none";
+      document.querySelector(".icon").style.display = "block";
     } else {
-      document.getElementById('nav').style.display = 'block';
-      document.querySelector('.icon').style.display = 'none';
+      document.getElementById("nav").style.display = "block";
+      document.querySelector(".icon").style.display = "none";
     }
   }
 
@@ -18,4 +18,29 @@ export const observer = (element) => {
   } else {
     window.addEventListener("resize", handleResize);
   }
+};
+
+export const event_press = (container) => {
+  window.addEventListener("keyup", function (event) {
+    let width_container = container.offsetWidth;
+    console.log(`width ${width_container}`);
+    if (width_container <= 1140 && width_container >= 360) {
+      if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
+        // Se ha presionado una tecla de flecha
+        if (event.code === "ArrowLeft") {
+          if (width_container > 360) {
+            container.style.width =
+              width_container !== 960
+                ? width_container - 180 + "px"
+                : width_container - 240 + "px";
+          }
+        } else {
+          container.style.width =
+            width_container !== 720
+              ? width_container + 180 + "px"
+              : width_container + 240 + "px";
+        }
+      }
+    }
+  });
 };
--- ui_components_design/navbar_01/js/script.js
+++ ui_components_design/navbar_01/js/script.js
@@ -1,7 +1,9 @@
-import { observer } from "./container-resize.js";
+import { event_press, observer } from "./container-resize.js";
 
 // Observar el evento resize del elemento
 /**
  * element: Elemento a observar
  */
-observer(document.getElementById('header'));
+observer(document.getElementById("header"));
+
+event_press(document.getElementById("header"));
