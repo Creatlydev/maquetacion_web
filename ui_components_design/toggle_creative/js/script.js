b113630c52494e9f2c1106fc8048a91006aaa304
Creatlydev <creatlydev@gmail.com>
2023-07-01 17:06:41 -0500

    Nuevo toggle switch random

---
+++ ui_components_design/toggle_creative/js/script.js
@@ -0,0 +1,9 @@
+import { event_press, observer } from "./container-resize.js";
+
+// Observar el evento resize del elemento
+/**
+ * element: Elemento a observar
+ */
+// observer(document.getElementById("header"));
+
+// event_press(document.getElementById("header"));
