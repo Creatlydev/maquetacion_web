a352454fa87917f758423216543b0503df7c0ef0
Creatlydev <creatlydev@gmail.com>
2023-07-25 17:15:44 -0500

    Se agrego componente c-tooltip

--- sidebar/js/script.js
+++ sidebar/js/script.js
@@ -3,28 +3,32 @@
 const ICON_MENU = document.getElementById('js-menu');
 const ASIDE = document.getElementById('js-aside');
 const BODY = document.getElementById('js-body');
+const ICON_COLLAPSE = document.getElementById('js-icon-collape-aside');
 
 ICON_MENU.addEventListener('click', () => {
-    ASIDE.classList.toggle('js-scale-aside');
-    BODY.classList.toggle('js-translate-body');
+    ASIDE.classList.toggle('c-aside--visible');
 })
 
-
+ICON_COLLAPSE.addEventListener('click', () => {
+    ASIDE.classList.toggle('c-aside--expand')
+    BODY.classList.toggle('c-body--translate')
+})
 
 // ESCUCHAR EL EVENTO RESIZE
 window.addEventListener('resize', () => {
     if (window.innerWidth > 760) {
-        ASIDE.classList.add('js-scale-aside');
-        BODY.classList.add('js-translate-body');
+        ASIDE.classList.add('c-aside--visible');
+        if (ASIDE.classList.contains('c-aside--expand')) {
+            ASIDE.classList.remove('c-aside--expand')
+            BODY.classList.remove('c-body--translate');
+        }
     } else {
-        ASIDE.classList.remove('js-scale-aside');
-        BODY.classList.remove('js-translate-body');
+        ASIDE.classList.remove('c-aside--visible');
+        BODY.classList.remove('c-body--translate');
     }
 })
 
 
 
 
-
-
-
+ 
